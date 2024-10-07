86010b87fc98606223bca88c319bcf078638df82
Nero <161507518+nerolyl@users.noreply.github.com>
2024-10-07 23:33:34 +0300

    chart

--- js/chart.js
+++ js/chart.js
@@ -1,38 +1,22 @@
 var ctx = document.getElementById('myChart').getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-        datasets: [{
-            label: 'Data',
-            data: [1300, 1200, 2800, 2200, 600, 1400, 1300],
-            backgroundColor: [
-                'rgba(75, 0, 130, 0.8)', // Deep purple color
-                'rgba(75, 0, 130, 0.8)',
-                'rgba(75, 0, 130, 0.8)',
-                'rgba(75, 0, 130, 0.8)',
-                'rgba(75, 0, 130, 0.8)',
-                'rgba(75, 0, 130, 0.8)',
-                'rgba(75, 0, 130, 0.8)'
-            ],
-            borderColor: [
-                'rgba(75, 0, 130, 1)',
-                'rgba(75, 0, 130, 1)',
-                'rgba(75, 0, 130, 1)',
-                'rgba(75, 0, 130, 1)',
-                'rgba(75, 0, 130, 1)',
-                'rgba(75, 0, 130, 1)',
-                'rgba(75, 0, 130, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true,
-                max: 3000
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+                datasets: [{
+                    label: 'calorie',
+                    data: [1300, 1200, 2800, 2200, 600, 1400, 1300],
+                    backgroundColor: '#5A246B',
+                    borderColor: 'rgba(75, 0, 130, 1)',
+                    
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true,
+                        suggestedMax: 3000
+                    }
+                }
             }
-        }
-    }
-});
+        });
