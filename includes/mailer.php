5276f946701242ec4f649a4a67c3b35afcd82ad2
Araragi-Noraneko <araraginoraneko@gmail.com>
2024-10-18 21:23:25 +0300

    FIXED LEAK to our email STMP password

---
+++ includes/mailer.php
@@ -0,0 +1,23 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$mail = new PHPMailer(true);
+
+$mail->isSMTP();
+$mail->SMTPAuth = true;
+
+$mail->Host = 'smtp.gmail.com';
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+$mail->Port = 587;
+$mail->Username = "";
+$mail->Password = "";
+
+$mail->isHTML(true);
+
+return $mail;
+
+
---
+++ python/test.py
@@ -0,0 +1,3 @@
+#! C:/Users/arara/AppData/Local/Programs/Python/Python313/python.exe
+print("Content-Type: text/html\n")
+print("<html><body><h1>Hello, World!</h1></body></html>")   
